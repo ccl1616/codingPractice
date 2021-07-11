b20c170e03e73ede8e6dbb9c35b96b65e9d09d61
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-07-11 23:08:09 +0800

    71. stuck

---
+++ leetcode/71_stack.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str1 = "apple";
+    string str2 = "foo";
+    str1.append(str2);
+    cout << str1 << endl;
+}
+
+// i give up on this q
