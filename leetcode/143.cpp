940e2a9f3ecc385f4fd3911857f646d2c5910f9e
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-07-12 00:07:11 +0800

    143. WA

--- leetcode/143.cpp
+++ leetcode/143.cpp
@@ -8,11 +8,12 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
 };
 
-// first try (failed)
+// first try (WA)
+// failed reason: cannot determine the stopiing time just by comparing ptr and stacl.top()
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        if(head = nullptr) return;
+        if( (!head) || (!head->next) || (!head->next->next) ) return;
         
         std::stack<ListNode*> ns; //node stack
         ListNode* ptr = head;
@@ -33,6 +34,10 @@ public:
                 topnode->next = nxt;
                 ptr = nxt;
             }
+            else if(ptr->next->val == topnode->val){
+                ptr->next->next = nullptr;
+                return;
+            }
             else {
                 ptr->next = nullptr;
                 return;
@@ -40,5 +45,34 @@ public:
         }
         
         return;
+    }
+};
+
+// sol
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if ((!head) || (!head->next) || (!head->next->next)) return; // Edge cases
+        
+        stack<ListNode*> my_stack;
+        ListNode* ptr = head;
+        int size = 0;
+        while (ptr != NULL) // Put all nodes in stack
+        {
+            my_stack.push(ptr);
+            size++;
+            ptr = ptr->next;
+        }
+        
+        ListNode* pptr = head;
+        for (int j=0; j<size/2; j++) // Between every two nodes insert the one in the top of the stack
+        {
+            ListNode *element = my_stack.top();
+            my_stack.pop();
+            element->next = pptr->next;
+            pptr->next = element;
+            pptr = pptr->next->next;
+        }
+        pptr->next = NULL;
     }
 };
