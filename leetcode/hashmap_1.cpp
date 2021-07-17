a17beb5c20029b8d6f28ae21035380802c407e2d
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-07-17 23:55:39 +0800

    1. trying

---
+++ leetcode/hashmap_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+  
+using namespace std;
+
+  vector<int> twoSum(vector<int>& nums, int target) {
+    std::unordered_map<int, std::size_t> tmp;
+    for (std::size_t i = 0; i < nums.size(); ++i) {
+      if (tmp.count(target - nums[i])) {
+        return {tmp[target - nums[i]], i};
+      }
+      tmp[nums[i]] = i;
+    }
+    return {nums.size(), nums.size()};
+  }
