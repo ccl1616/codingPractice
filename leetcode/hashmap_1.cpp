e057666c807b6b5b5a1e6eeed1c968881f120985
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-07-17 23:57:15 +0800

    tring

--- leetcode/hashmap_1.cpp
+++ leetcode/hashmap_1.cpp
@@ -8,13 +8,21 @@
   
 using namespace std;
 
-  vector<int> twoSum(vector<int>& nums, int target) {
-    std::unordered_map<int, std::size_t> tmp;
-    for (std::size_t i = 0; i < nums.size(); ++i) {
-      if (tmp.count(target - nums[i])) {
-        return {tmp[target - nums[i]], i};
-      }
-      tmp[nums[i]] = i;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+            // Keys are the array values and values are the associated array indices
+            unordered_map<int, int> hash; // Use as a hash table, instead of creating one
+            vector<int> result;
+            for (int i = 0; i < nums.size(); i++)
+                if (hash.count(target - nums[i])) // If the partner of this value to reach the target sum has been saved already
+                {
+                    result.push_back(hash[target - nums[i]]); // Get index of other value
+                    result.push_back(i);
+                    return result;
+                }
+                else // Pair has not yet been found, so save value to hash table
+                    hash[nums[i]] = i;
+            return result;
     }
-    return {nums.size(), nums.size()};
-  }
+};
