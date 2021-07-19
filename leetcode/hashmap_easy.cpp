27a542623beaf080e3dd7d789d6345670be63307
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-07-19 22:44:06 +0800

    349. update

--- leetcode/hashmap_easy.cpp
+++ leetcode/hashmap_easy.cpp
@@ -6,6 +6,7 @@
 using namespace  std;
 
 // 349
+// my first try, use 2 maps
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
@@ -19,6 +20,27 @@ public:
             if(map.count(i) && !map2.count(i)) {
                 ret.push_back(i);
                 map2[i] = 1;
+            }
+        }
+        return ret;
+    }
+};
+
+// me using 1 map, but the memory seems same
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ret;
+        unordered_map<int, int> map;
+        for(auto x: nums1) {
+            if(!map.count(x)) map[x] = 1;
+        }
+        for(auto i: nums2) {
+            if(map.count(i) ) {
+                if(map[i]) {
+                    ret.push_back(i);
+                    map[i] = 0;
+                }
             }
         }
         return ret;
