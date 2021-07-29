7e9681bcca2f56488bb5eeb395d0108c676e7a5d
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-07-29 15:47:56 +0800

    105. doing

--- leetcode/tree/medium.cpp
+++ leetcode/tree/medium.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#include <unordered_map>
 using namespace std;
 
 struct TreeNode {
@@ -201,3 +202,28 @@ private:
     vector<int> tree;
 };
 // ===============================================================================================
+
+// 105
+// construct binary tree by preorder & inorder
+// sol1 use unordered map
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& P, vector<int>& I) {
+        unordered_map<int, int> M;
+        // put them into map
+        for (int i = 0; i < I.size(); i++)
+            M[I[i]] = i;
+        return splitTree(P, M, 0, 0, I.size()-1);
+    }
+    
+private:
+    TreeNode* splitTree(vector<int>& P, unordered_map<int, int>& M, int pix, int ileft, int iright) {
+        int rval = P[pix], imid = M[rval];
+        TreeNode* root = new TreeNode(rval);            
+        if (imid > ileft)
+            root->left = splitTree(P, M, pix+1, ileft, imid-1);
+        if (imid < iright)
+            root->right = splitTree(P, M, pix+imid-ileft+1, imid+1, iright);
+        return root;
+    }
+};
