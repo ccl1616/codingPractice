a409416b72aa2c8bd8d2139d3edd2195f281e4e4
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-08-01 00:17:21 +0800

    198. done

--- toolbox/test.cpp
+++ toolbox/test.cpp
@@ -7,12 +7,9 @@ void func(int& j)
     return;
 }
 int main() {
-    vector< vector<int> > arr;
-    vector<int> vect{ 10, 20, 30 };
-    vector<int> vect2{ 10, 20, 30 };
-    arr.push_back(vect);
-    arr.push_back(vect2);
-    cout << arr.size()<< endl; // row num
-    cout << arr[0].size()<< endl;
+    int val = 10;
+    int* dp = new int[val];
+    dp[5] = 50;
+    cout << dp[5] << endl;
     return 0;
 }
