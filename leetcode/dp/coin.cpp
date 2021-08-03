b5b134684194d3c3e1429d767cc4f73dc374bc35
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-08-03 22:25:12 +0800

    518. done

--- leetcode/dp/coin.cpp
+++ leetcode/dp/coin.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 // 322.
 // dp[i]: minimum #coins for $i
-
 // back to SWE, bottom-up
 // vector version
 class Solution {
@@ -41,5 +41,52 @@ public:
             }
         }
         return dp[amount] > amount ?-1:dp[amount];
+    }
+};
+
+// ====================================================================================
+// 518
+/*
+    dp[i] = #combination of $i
+*/
+// 2D
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        int dp[n+1][amount+1];
+        dp[0][0] = 1;
+        for(int i = 1; i <= amount; i ++) dp[0][i] = 0;
+        for(int i = 1; i <= n; i ++) dp[i][0] = 1;
+
+        for(int i = 1; i <= n; i ++) {
+            for(int j = 1; j <= amount; j ++) {
+                // don't use this coin + use this coin
+                if(coins[i-1] <= j)
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][amount];
+    }
+};
+
+// 1D
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        if(amount == 0)
+            return 1;
+        // if(coins.size() == 0)
+        //     return 0;
+        // vector<int> dp(amount + 1, 0);
+        int dp[amount+1];
+        dp[0] = 1;
+        for(int i = 0; i < coins.size(); ++ i){
+            for(int j = coins[i]; j <= amount; ++ j){
+                dp[j] += dp[j - coins[i]];
+            }
+        }
+        return dp[amount];
     }
 };
--- toolbox/test.cpp
+++ toolbox/test.cpp
@@ -2,10 +2,17 @@
 using namespace std;
 
 int main() {
-    map<string,bool> m;
-    m["hi"] = 1;
-    auto iter = m.find("hi");
-    if(iter != m.end()) cout << "1" << endl;
-    else cout << "0" << endl;
+    int r = 5;
+    int c = 10;
+    int arr[r][c];
+    for(int i = 0 ; i < r; i ++){
+        for(int j = 0; j < c; j ++)
+            arr[i][j] = c*i + j;
+    }
+    for(int i = 0; i < r; i ++) {
+        for(int j = 0; j < c; j ++)
+            cout << arr[i][j] << " ";
+        cout << endl;
+    }
     return 0;
 }
