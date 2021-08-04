476be273c3b30aab5fc3fc7b376fa1d8debc8020
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-08-04 23:27:43 +0800

    153. done

--- leetcode/binarySearch/arrayRelated.cpp
+++ leetcode/binarySearch/arrayRelated.cpp
@@ -11,7 +11,7 @@ public:
     }
 };
 
-// jian
+// jian, real binary search
 class Solution {
 public:
     int findMin(vector<int>& nums) {
@@ -19,10 +19,27 @@ public:
         while (l < r && nums[l] > nums[r]) {
             int m = l + (r - l) / 2;
             if (nums[m] > nums[r]) {
+                // little number is on the right
                 l = m + 1;
             } else {
+                // little number is on the left
                 r = m;
             }
+        }
+        return nums[l];
+    }
+};
+
+// me after sol
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size()-1;
+        while(l < r) {
+            int m = l + (r - l) / 2;
+            if(nums[m] > nums[r]) l = m+1;
+            else r = m;
         }
         return nums[l];
     }
