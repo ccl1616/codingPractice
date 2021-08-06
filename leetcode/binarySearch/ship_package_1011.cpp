05b48bc314be5d85a9963908d5e5feda1b846ed9
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-08-06 23:46:16 +0800

    1011. come back later

--- leetcode/binarySearch/ship_package_1011.cpp
+++ leetcode/binarySearch/ship_package_1011.cpp
@@ -1,10 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 1011 
+// sol
 class Solution {
 public:
-    int shipWithinDays(vector<int>& weights, int days) {
-        // return capacity
-        // hello
+    int countDays(vector<int>& ws, int tot_cap, int cur_cap = 0, int res = 1) {
+        for (auto w : ws) {
+            cur_cap += w;
+            if (cur_cap > tot_cap) ++res, cur_cap = w;
+        }
+        return res;
+    }
+    int shipWithinDays(vector<int>& ws, int D) {
+        auto r = accumulate(begin(ws), end(ws), 0);
+        auto l = max(r / D, *max_element(begin(ws), end(ws)));
+        while (l < r) {
+            auto m = (l + r) / 2;
+            if (countDays(ws, m) <= D) r = m;
+            else l = m + 1;
+        }
+        return l;
     }
 };
