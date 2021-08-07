b920d7aaf7ca7b8c7e70133a0604f2102d0fc0ff
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-08-07 23:17:28 +0800

    779. done

--- toolbox/test.cpp
+++ toolbox/test.cpp
@@ -2,17 +2,7 @@
 using namespace std;
 
 int main() {
-    int r = 5;
-    int c = 10;
-    int arr[r][c];
-    for(int i = 0 ; i < r; i ++){
-        for(int j = 0; j < c; j ++)
-            arr[i][j] = c*i + j;
-    }
-    for(int i = 0; i < r; i ++) {
-        for(int j = 0; j < c; j ++)
-            cout << arr[i][j] << " ";
-        cout << endl;
-    }
+    int a = 1;
+    cout << ~a << endl;
     return 0;
 }
