c6870d32e4c31a06dd00854406e4f88995f52841
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-08-11 23:13:16 +0800

    78. done

--- leetcode/greedy_backtracking/subset_78.cpp
+++ leetcode/greedy_backtracking/subset_78.cpp
@@ -18,12 +18,52 @@ public:
     }
 };
 
-// after sol
+// sol, backtracking
 class Solution {
 public:
+    vector<vector<int>> ans;
+    
+    void sub(vector<int> &nums, int i, vector<int> temp)
+    {
+        if(i==nums.size())
+        {
+            ans.push_back(temp);
+            return;
+        }
+        
+        sub(nums, i+1, temp);
+        temp.push_back(nums[i]);
+        sub(nums, i+1, temp);
+    }
+    
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>arr = {{}};
-        
-        
+        vector<int> temp;       
+        sub(nums, 0, temp); // or sub(nums, 0, vector<int> {});
+        return ans;
+    }
+};
+
+// backtracking, after sol
+class Solution {
+public:
+    vector<vector<int>> ans;
+    
+    void bt(vector<int> &nums, int i, vector<int> temp)
+    {
+        // base case
+        if(i == nums.size()) {
+            ans.push_back(temp);
+            return;
+        }
+        // don't-add or add
+        bt(nums, i+1, temp);
+        temp.push_back(nums[i]);
+        bt(nums, i+1, temp);
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;       
+        bt(nums, 0, temp); 
+        return ans;
     }
 };
--- toolbox/test.cpp
+++ toolbox/test.cpp
@@ -1,8 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<vector<int> > ans;
+int counter;
+
+void sub(vector<int> &nums, int i, vector<int> temp)
+{
+    if(i==nums.size())
+    {
+        counter ++;
+        ans.push_back(temp);
+        return;
+    }
+    
+    sub(nums, i+1, temp);
+    temp.push_back(nums[i]);
+    sub(nums, i+1, temp);
+}
+
+vector<vector<int>> subsets(vector<int>& nums) {
+    vector<int> temp;       
+    sub(nums, 0, temp); // or sub(nums, 0, vector<int> {});
+    return ans;
+}
+
 int main() {
-    int a = 1;
-    cout << ~a << endl;
+    vector<vector<int> > arr;
+    vector<int> tmp = {1,2,3};
+    vector<vector<int> > ret = subsets(tmp);
+    for(auto x: ret) {
+        for(int i = 0; i < x.size(); i ++) 
+            cout << x[i] << ",";
+        cout << endl;
+    }
+    cout << "count: " << counter << endl;
     return 0;
 }
