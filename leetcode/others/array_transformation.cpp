1dc27949e268243e13f5e19efb41f4ef1b38da36
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-08-14 23:47:01 +0800

    283 sol2

--- leetcode/others/array_transformation.cpp
+++ leetcode/others/array_transformation.cpp
@@ -2,34 +2,25 @@
 using namespace std;
 
 // 283
-// official sol
+// brutal force, after official sol
+// faster than 89%
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-
-        // Count the zeroes
-        int numZeroes = 0;
-        for (int i = 0; i < n; i++) {
-            numZeroes += (nums[i] == 0);
+        // count the zeros
+        int size = nums.size();
+        int zero_count = 0;
+        vector<int> ret;
+        for(int i = 0; i < size; i ++){
+            if(nums[i] == 0) zero_count ++;
+            else ret.push_back(nums[i]);
         }
-
-        // Make all the non-zero elements retain their original order.
-        vector<int> ans;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) {
-                ans.push_back(nums[i]);
-            }
-        }
-
-        // Move all zeroes to the end
-        while (numZeroes--) {
-            ans.push_back(0);
-        }
-
-        // Combine the result
-        for (int i = 0; i < n; i++) {
-            nums[i] = ans[i];
-        }
+        // push back zeros
+        while(zero_count--) 
+            ret.push_back(0);
+        
+        for(int i = 0; i < size; i ++) 
+            nums[i] = ret[i];
+        
     }
 };
