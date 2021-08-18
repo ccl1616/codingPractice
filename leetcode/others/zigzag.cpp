54ea3a58630f66bfee6d3f72b8e2342bedb0646f
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-08-19 00:06:04 +0800

    6. done

--- leetcode/others/zigzag.cpp
+++ leetcode/others/zigzag.cpp
@@ -2,9 +2,32 @@
 using namespace std;
 
 // 6.
-class Solution {
-public:
-    string convert(string s, int numRows) {
-        
+// my code !
+string convert(string s, int numRows) {
+    if(numRows == 1) return s;
+    vector<string> vec;
+    string ret = "";
+    for(int i = 0; i <= numRows; i ++) {
+        vec.push_back("");
     }
-};
+    int p = 1;
+    bool increase = true;
+    for(int i = 0; i < s.length(); i ++) {
+        // cout << p << endl;
+        vec[p] += s[i];
+        if(p == numRows) increase = increase ?false :true;
+        if(p == 1) {
+            p += 1; 
+            increase = true;
+        }
+        else p += increase ?1 :-1;
+    }
+    for(auto x:vec)
+        ret += x;
+    return ret;
+}
+
+int main() {
+    string test = "PAYPALISHIRING";
+    cout << convert(test, 4) << endl;
+}
