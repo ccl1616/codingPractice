3337a6729a958a224f618c4d72c26b6b07c0cb5f
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-08-19 23:07:10 +0800

    update

--- leetcode/others/zigzag.cpp
+++ leetcode/others/zigzag.cpp
@@ -7,20 +7,19 @@ string convert(string s, int numRows) {
     if(numRows == 1) return s;
     vector<string> vec;
     string ret = "";
-    for(int i = 0; i <= numRows; i ++) {
+    for(int i = 0; i <= numRows; i ++) 
         vec.push_back("");
-    }
-    int p = 1;
+    
+    int row = 1;
     bool increase = true;
     for(int i = 0; i < s.length(); i ++) {
-        // cout << p << endl;
-        vec[p] += s[i];
-        if(p == numRows) increase = increase ?false :true;
-        if(p == 1) {
-            p += 1; 
+        vec[row] += s[i];
+        if(row == numRows) increase = increase ?false :true;
+        if(row == 1) {
+            row += 1; 
             increase = true;
         }
-        else p += increase ?1 :-1;
+        else row += increase ?1 :-1;
     }
     for(auto x:vec)
         ret += x;
@@ -28,6 +27,6 @@ string convert(string s, int numRows) {
 }
 
 int main() {
-    string test = "PAYPALISHIRING";
-    cout << convert(test, 4) << endl;
+    string test = "abcd";
+    cout << convert(test, 2) << endl;
 }
