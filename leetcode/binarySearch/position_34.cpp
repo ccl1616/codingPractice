d048603fe9a779b98a087e33aa24e59b7a29167c
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-08-20 23:15:39 +0800

    9. done

---
+++ leetcode/binarySearch/position_34.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+    }
+};
--- toolbox/test.cpp
+++ toolbox/test.cpp
@@ -1,40 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-int myAtoi(string s) {
-    long long int num = 0;
-    bool isNeg = false;
+void func(int x) {
+    int y = 0;
     int i = 0;
-    // leading white space
-    while( (int)s[i] == 32 ) i ++;
-    while( i < s.size() ) {
-        
-        if( s[i] == '-' ) {
-            if( !isdigit(s[i+1]) )
-                return 0;
-            isNeg = true;
-        }
-        else if( s[i] == '+' && s[i+1] == '-' )
-            return 0;
-        else if( !isdigit(s[i]) && s[i] != '+' ) 
-            break;
-        else if( s[i] != '+') {
-            num *= 10;
-            num += s[i]-'0';
-        }
+    stack<int> st;
+    while( x != 0) {
+        cout << x % 10 << endl;
+        y += (x % 10) * pow(10,i);
         i ++;
+        x /= 10;
     }
-    if( num > numeric_limits<int>::max() ) {
-        num = isNeg ?numeric_limits<int>::min():numeric_limits<int>::max();
-        return num;
-    }
-    return isNeg ? (-1)*num : num;
+    cout << y << endl;
 }
 
 int main() {
-    int sign = 1;
-    sign = (3 > 2) ?:-1;
-    cout << sign << endl;
+    func(1234567890);
     return 0;
 }
