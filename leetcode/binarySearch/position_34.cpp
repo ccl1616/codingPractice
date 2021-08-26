40a0b2f4e1009c41856c17700a4f57638e8f01ca
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-08-26 23:50:07 +0800

    34. done

--- leetcode/binarySearch/position_34.cpp
+++ leetcode/binarySearch/position_34.cpp
@@ -11,7 +11,8 @@ public:
         int low = 0, high = nums.size()-1, result = -1;
         while(low <= high)
         {
-            int mid = (low + high)/2;
+            // int mid = (low + high)/2;
+            int mid = low + (high - low)/2;
             if(nums[mid] == target)
             {
                 result = mid;
@@ -23,7 +24,33 @@ public:
         }
         return result;
     }
-    
+    /*
+        first occurence: the first time that the target appears in the array
+    */
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return {BinarySearch(nums,target,true),BinarySearch(nums,target,false)};
+    }
+};
+// after sol, almost same
+class Solution {
+public:
+    int BinarySearch(vector<int>& nums, int target, bool firstOccurence)
+    {
+        int left = 0;
+        int right = nums.size()-1;
+        int ret = -1;
+        while(left <= right) {
+            int mid = left + (right - left)/2;
+            if(nums[mid] == target) {
+                ret = mid;
+                if(firstOccurence) right = mid - 1;
+                else left = mid+1;
+            }
+            else if(nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return ret;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
         return {BinarySearch(nums,target,true),BinarySearch(nums,target,false)};
     }
