de65d5e58f7d2218363d9d0a7e75e5f0e669a845
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-08-30 23:10:24 +0800

    509. done

---
+++ leetcode/others/fib.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 509. easy, simple fib
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        return fib(n-1) + fib(n-2);
+    }
+};
