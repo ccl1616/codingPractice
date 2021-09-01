6b6efd97328276ae7f840dab677d887b17befcc5
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-09-01 23:51:56 +0800

    22. update

--- leetcode/backtracking/parentheses_22.cpp
+++ leetcode/backtracking/parentheses_22.cpp
@@ -23,3 +23,20 @@ public:
         if(right < left) func( str+")", left, right+1);
     }
 };
+
+class Solution {
+public:
+    vector<string> ret;
+    vector<string> generateParenthesis(int n) {
+        func("", 0, 0, n);
+        return ret;
+    }
+    void func(string str, int left, int right, int n) {
+        if(str.length() == n*2) {
+            ret.push_back(str);
+            return;
+        }
+        if(left < n) func( str+"(", left+1, right, n);
+        if(right < left) func( str+")", left, right+1, n);
+    }
+};
