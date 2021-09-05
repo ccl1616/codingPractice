9cda3e119ca9fe7397ec29ff6f776071c7b7ea96
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-09-05 22:26:58 +0800

    update

--- leetcode/tree/easy.cpp
+++ leetcode/tree/easy.cpp
@@ -25,9 +25,7 @@ class Solution {
 public:
     int maxDepth(TreeNode* root) {
         if(!root) return 0;
-        else {
-            return 1 + max(maxDepth(root->left),maxDepth(root->right));
-        }
+        else return 1 + max(maxDepth(root->left),maxDepth(root->right));
     }
 };
 
