e81e041fdbda4c13ca5a56c8008f43fb65a9d79c
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-09-14 17:23:58 +0800

    daily done

--- leetcode/0daily/reverseOnlyLetters.cpp
+++ leetcode/0daily/reverseOnlyLetters.cpp
@@ -1,2 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int l = 0;
+        int r = s.size()-1;
+        while(l < r && l < s.size() ) {
+            if( !isalpha(s[l]) ) {
+                l ++;
+            }
+            if( !isalpha(s[r])) {
+                r --;
+            }
+
+            if( isalpha(s[l])&&isalpha(s[r]) ) {
+                // swap
+                char tmp = s[r];
+                s[r] = s[l];
+                s[l] = tmp;
+                l ++;
+                r --;
+            }
+        }
+        return s;
+    }
+};
