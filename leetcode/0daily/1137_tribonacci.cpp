4c3dc9cdb796f7c953bdaba1d39d92da9b6573de
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-09-24 23:09:30 +0800

    0924 done

---
+++ leetcode/0daily/1137_tribonacci.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        int arr[38];
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 1;
+        int ret = 0;
+        if(n <= 2) {
+            if(n == 0) return 0;
+            else return 1;
+        }
+        for(int i = 3; i <= n; i ++) 
+            arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
+        return arr[n];
+    }
+};
