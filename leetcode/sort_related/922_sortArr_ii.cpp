657d34d290b1fda5220da5dd0d34acfa5c997aef
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-09-28 22:08:16 +0800

    0928 update

--- leetcode/sort_related/922_sortArr_ii.cpp
+++ leetcode/sort_related/922_sortArr_ii.cpp
@@ -24,3 +24,16 @@ public:
         return nums;
     }
 };
+
+// two pointer, way more smarter
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        for( int i = 0, j = 1; i < nums.size(), j < nums.size(); i += 2, j += 2) {
+            while(i < nums.size() && !(nums[i] % 2)) i += 2; // find odd num
+            while(j < nums.size() && (nums[j] % 2) ) j += 2; // find even num
+            if(i < nums.size() && j < nums.size() ) swap(nums[i], nums[j]);
+        }
+        return nums;
+    }
+};
