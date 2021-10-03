410c39d7a6823d5747ef9df3c914e040d4966292
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-10-03 22:51:32 +0800

    update

--- leetcode/dp/max_subarr_53.cpp
+++ leetcode/dp/max_subarr_53.cpp
@@ -8,9 +8,31 @@ public:
         int ret = nums[0];
         int cur = nums[0];
         for(int i = 1; i < nums.size(); i ++) {
-            cur = max(nums[i], cur+nums[i]);
+            cur = max(nums[i], cur+nums[i]); //(start a new sub arr, current best + myself)
             if(cur > ret) ret = cur;
         }
         return ret;
     }
 };
+
+int maxSubArray(vector<int>& nums) {
+    int low = 0;
+    int high = 0;
+    int ret = nums[0];
+    int cur = nums[0];
+    for(int i = 1; i < nums.size(); i ++) {
+        cur = max(nums[i], cur+nums[i]); //(start a new sub arr, current best + myself)
+        if(cur > ret) {
+            ret = cur;
+            if(cur == nums[i]) low = i;
+            else high = i;
+        }
+    }
+    cout << "low:" << low << " , high:" << high << endl;
+    return ret;
+}
+int main() 
+{
+    vector<int> v = {-2,1,-3,4,-1,2,1,-5,4};
+    cout << "max: " << maxSubArray(v) << endl;
+}
