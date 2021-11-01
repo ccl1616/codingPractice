2b9105582b56fa66c3fd20b03c220d90b0457aca
ccl1616 <chia16lin@gapp.nthu.edu.tw>
2021-11-02 00:02:58 +0800

    118. done

--- leetcode/dp/pascal_118.cpp
+++ leetcode/dp/pascal_118.cpp
@@ -38,33 +38,14 @@ public:
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        // heap overflow
-        /*
-        vector<vector<int>> ret;
-        if(numRows == 1) {
-            vector<int> cur;
-            cur.push_back(1);
-            ret.push_back(cur);
-            return ret;
-        }
-        vector<int> t;
-        t.push_back(1);
-        t.push_back(1);
-        ret.push_back(t);
-        if(numRows == 2) return ret;
-
-        for(int i = 2; i < numRows; i ++) {
-            vector<int> cur;
-            cur.push_back(1);
-            // size = i+1
-            // should count 1~i
-            for(int j = 1; j <= i; i ++) {
-                cur.push_back( ret[i-1][j-1]+ret[i-1][j] );
-            }
-            cur.push_back(1);
-            ret.push_back(cur);
+        vector<vector<int>> ret(numRows);
+        for(int i = 0; i < numRows; i ++) {
+            ret[i].resize(i+1);
+            ret[i][0] = 1;
+            ret[i][i] = 1;
+            for(int j = 1; j < i; j ++)
+                ret[i][j] = ret[i-1][j-1] + ret[i-1][j];
         }
         return ret;
-        */
     }
 };
